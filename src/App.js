e98da7eabe7cd501bf9bcced3db644a44ce32a11
avijit <avijit.erptech@gmail.com>
2022-08-05 18:58:04 +0530

    add style links

--- src/App.js
+++ src/App.js
@@ -8,18 +8,27 @@ import { saveState } from "../src/utility/browser-storage";
 import { debounce } from "debounce";
 
 // css
+import "../src/assets/css/main.css"
 import "../src/assets/css/plugins/animate.min.css"
-import "../src/assets/css//main.css?v=5.5"
+import "../src/assets/css/vendors/bootstrap.min.css"
+import "../src/assets/css/vendors/normalize.css"
+import "../src/assets/css/vendors/normalize.css"
+import "../src/assets/css/vendors/uicons-regular-straight.css"
+import "../src/assets/css/vendors/uicons-regular-straight.css"
+import "../src/assets/css/plugins/animate.min.css"
+import "../src/assets/css/plugins/magnific-popup.css"
+import "../src/assets/css/plugins/perfect-scrollbar.css"
+import "../src/assets/css/plugins/select2.min.css"
+import "../src/assets/css/plugins/slick.css"
+import "../src/assets/css/plugins/slider-range.css"
 
 //  JS
 
-
-
 // import "../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"
 // import "../src/assets/js/vendor/modernizr-3.6.0.min.js"
-import "../src/assets/js/vendor/jquery-3.6.0.min.js"
+// import "../src/assets/js/vendor/jquery-3.6.0.min.js"
 // import "../src/assets/js/vendor/jquery-migrate-3.3.0.min.js"
-import "../src/assets/js/vendor/bootstrap.bundle.min.js"
+// import "../src/assets/js/vendor/bootstrap.bundle.min.js"
 // import "../src/assets/js/plugins/slick.js"
 // import "../src/assets/js/plugins/jquery.syotimer.min.js"
 // import "../src/assets/js/plugins/waypoints.js"
